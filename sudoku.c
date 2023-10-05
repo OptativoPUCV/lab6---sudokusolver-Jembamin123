90d54fe138b116d6e8c4f9f389d01e3b98c16669
Replit user <>
2023-10-05 20:11:45 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,30 +44,36 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-List* get_adj_nodes(Node* current_node) {
-    List* adj_nodes = createList();
+  int auxCol[9][10];       //  números en columnas
+  int auxFil[9][10];       //  números en filas
+  int auxSubMa[3][3][10];  //  números en submatrices
+  int num;
+  
+  for(int fil=0; fil<9; fil++) {
+    for(int col=0; col<9; col++) {
+      num=n->sudo[fil][col];
+      if(num<1 || num>9){
+        return 0; 
+      }
+      
+      if(auxFil[fil][num]==1){
+        return 0; 
+      }
+      auxFil[fil][num]=1;
+      if(auxCol[col][num]==1){
+        return 0;  
+      }
+      auxCol[col][num]=1;
 
-    for (int fila = 0; fila < 9; fila++) {
-        for (int col = 0; col < 9; col++) {
-            if (current_node->sudo[fila][col] == 0) {
-                for (int num = 1; num <= 9; num++) {
-                    Node* new_node = copy(current_node);
-                    new_node->sudo[fila][col] = num;
-
-                    if (is_valid(new_node)) {
-                        pushBack(adj_nodes, new_node);
-                    } else {
-                        free(new_node);  // Liberar memoria de nodos no válidos
-                    }
-                }
-                // Como se han agregado todos los posibles números en esta celda, podemos salir del bucle interno.
-                break;
-            }
-        }
+      int subMaFILL =fil/3;
+      int subMaCOL  =col/3;
+      if (auxSubMa[subMaFILL][subMaCOL][num]==1){
+        return 0;
+      }
+      auxSubMa[subMaFILL][subMaCOL][num]=1;
     }
-
-    return adj_nodes;
-
+  }
+  return 1;
 }
 
 
