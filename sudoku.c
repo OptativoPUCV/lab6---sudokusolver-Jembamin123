cf9d02e4080c776eb7cabd9123fb86e64b693348
Replit user <>
2023-10-05 20:05:39 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,37 +44,38 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-  int auxCol[9][10];       //  números en columnas
-  int auxFil[9][10];       //  números en filas
-  int auxSubMa[3][3][10];  //  números en submatrices
+  int auxCol[9][10] = {0};       // Números en columnas
+  int auxFil[9][10] = {0};       // Números en filas
+  int auxSubMa[3][3][10] = {0};  // Números en submatrices
   int num;
   
-  for(int fil=0; fil<9; fil++) {
-    for(int col=0; col<9; col++) {
-      num=n->sudo[fil][col];
-      if(num<1 || num>9){
+  for(int fil = 0; fil < 9; fil++) {
+    for(int col = 0; col < 9; col++) {
+      num = n->sudo[fil][col];
+      if(num < 1 || num > 9) {
         return 0; 
       }
       
-      if(auxFil[fil][num]==1){
+      if(auxFil[fil][num] == 1) {
         return 0; 
       }
-      auxFil[fil][num]=1;
-      if(auxCol[col][num]==1){
+      auxFil[fil][num] = 1;
+      if(auxCol[col][num] == 1) {
         return 0;  
       }
-      auxCol[col][num]=1;
+      auxCol[col][num] = 1;
 
-      int subMaFILL =fil/3;
-      int subMaCOL  =col/3;
-      if (auxSubMa[subMaFILL][subMaCOL][num]==1){
+      int subMaFILA = fil / 3;
+      int subMaCOL = col / 3;
+      if(auxSubMa[subMaFILA][subMaCOL][num] == 1) {
         return 0;
       }
-      auxSubMa[subMaFILL][subMaCOL][num]=1;
+      auxSubMa[subMaFILA][subMaCOL][num] = 1;
     }
   }
   return 1;
 }
+
 
 
 List* get_adj_nodes(Node* n){
