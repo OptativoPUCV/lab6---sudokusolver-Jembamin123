2654d7ec382df9f975406010a8df6cc8496f19fd
Replit user <>
2023-10-05 20:21:19 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,35 +44,7 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-  int auxCol[9][10]={0};       //  números en columnas
-  int auxFil[9][10]={0};       //  números en filas
-  int auxSubMa[3][3][10]={0};  //  números en submatrices
-  int num;
-  
-  for(int fil=0; fil<9; fil++) {
-    for(int col=0; col<9; col++) {
-      num=n->sudo[fil][col];
-      if(num<1 || num>9){
-        return 0; 
-      }
-      
-      if(auxFil[fil][num]==1){
-        return 0; 
-      }
-      auxFil[fil][num]=1;
-      if(auxCol[col][num]==1){
-        return 0;  
-      }
-      auxCol[col][num]=1;
 
-      int subMaFILL =fil/3;
-      int subMaCOL  =col/3;
-      if (auxSubMa[subMaFILL][subMaCOL][num]==1){
-        return 0;
-      }
-      auxSubMa[subMaFILL][subMaCOL][num]=1;
-    }
-  }
   return 1;
 }
 
