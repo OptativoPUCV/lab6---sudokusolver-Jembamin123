1ca35905220f6c1c649f449b6d0cf52d596185bc
Replit user <>
2023-10-09 02:37:23 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -104,6 +104,12 @@ List* get_adj_nodes(Node* n) {
     }
   }
 
+  if (!emptyFound) {
+    // Si no se encontraron celdas vacías, el nodo actual es un nodo final
+    // válido, agrégalo a la lista.
+    pushBack(list, n);
+  }
+
   return list;
 }
 
