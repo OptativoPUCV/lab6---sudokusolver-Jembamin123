5cabf9bc02d59a1dd56d01caea8d8666f5287e11
Replit user <>
2023-10-09 02:35:51 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -83,28 +83,33 @@ int is_valid(Node* n){
 
 
 
-List* get_adj_nodes(Node* n){
+List* get_adj_nodes(Node* n) {
   List* list = createList();
-  
-  for(int fil=0; fil<9; fil++){
-    for(int col=0; col<9; col++){
-      if(n->sudo[fil][col] == 0){
-        for(int num=1; num<=9; num++){
-          Node* adj_node=copy(n);
-          adj_node->sudo[fil][col]=num;
-          if(is_valid(adj_node)){
+  int foundEmptyCell = 0;  
+
+  for (int fil = 0; fil < 9; fil++) {
+    for (int col = 0; col < 9; col++) {
+      if (n->sudo[fil][col] == 0) {
+        foundEmptyCell = 1;  
+        for (int num = 1; num <= 9; num++) {
+          Node* adj_node = copy(n);
+          adj_node->sudo[fil][col] = num;
+          if (is_valid(adj_node)) {
             pushBack(list, adj_node);
-          } 
-          else{
+          } else {
             free(adj_node);
           }
         }
       }
     }
   }
+
+  if (!foundEmptyCell) {
+    pushBack(list, n);
+  }
+
   return list;
 }
-
 
 
 
