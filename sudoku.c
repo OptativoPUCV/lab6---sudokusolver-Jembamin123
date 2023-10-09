5102b7ce89d22c8c3465ad6cdd8722b1f2509c62
Replit user <>
2023-10-09 02:27:41 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -90,8 +90,8 @@ List* get_adj_nodes(Node* n){
     for(int col=0; col<9; col++){
       if(n->sudo[fil][col] == 0){
         for(int num=1; num<=9; num++){
-          Node* adj_node = copy(n);
-          adj_node->sudo[fil][col]=num;
+          Node* adj_node=copy(n);
+          adj_node->sudo[-1][-1]=num;
           if(is_valid(adj_node)){
             pushBack(list, adj_node);
           } 
